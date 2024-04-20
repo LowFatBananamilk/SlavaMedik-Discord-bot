984b00c07bafd9731ef8bfaa2930c4774d45c0ed
LowFatBananamilk <bananamilk420@gmail.com>
2024-04-20 23:59:44 +0900

    feat(Twitch Live Notification): :sparkles: compare live status from previous check to tell when the stream goes live or offline

--- bot.py
+++ bot.py
@@ -9,14 +9,29 @@ from twitchAPI.twitch import Twitch
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
-
+# todo: use CRON like time instead of intervals
 @tasks.loop(seconds=10)
 async def check_live():
     twitch = await Twitch(os.getenv('TWITCHCLIENTID'), os.getenv('TWITCHCLIENTSECRET'))
     results = twitch.get_streams(user_id='user_id_here')
     streams = [stream async for stream in results]
-    print('Stream offline' if len(streams) == 0 else 'Stream Online')
-    return False if len(streams) == 0 else True
+
+    check_live.is_live = True if len(streams) == 1 else False
+    
+    if not hasattr(check_live, 'previous_is_live'):
+        check_live.previous_is_live = check_live.is_live
+        return
+
+    if check_live.previous_is_live != check_live.is_live:
+        if check_live.is_live:
+            print('Stream has gone live.')
+        else:
+            print('Stream has gone offline.')
+    else:
+        if check_live.is_live:
+            print('Stream is live.')
+        else:
+            print('Stream is offline.')
 
 
 @client.event

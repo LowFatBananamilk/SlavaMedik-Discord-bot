86f86df6b98c1fa6c3e7b8950515fd49b214c9d6
LowFatBananamilk <bananamilk420@gmail.com>
2024-04-20 16:44:05 +0900

    feat: :tada: Initial commit

---
+++ bot.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+import discord
+
+intents = discord.Intents.default()
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+load_dotenv()
+client.run(os.getenv("DISCORDBOTTOKEN"))
